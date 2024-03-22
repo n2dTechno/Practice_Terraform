1c1d606f47057594fc00d0300cffc17d1c2cfd44
dinesh.bhat <dbhat1@wgu.edu>
2024-03-21 21:41:19 -0500

    code updated

--- Account/provider.tf
+++ Account/provider.tf
@@ -3,7 +3,7 @@ provider "databricks" {
   host          = "https://accounts.cloud.databricks.com"
   client_id     = var.databricks_client_id
   client_secret = var.databricks_client_secret
-  account_id    = "6828e5f2-b292-4e0a-945f-3de4b136f6ad"
+  account_id    = "276f23b2-904b-4bcc-922c-fa45276cdc91"
 }
 resource "databricks_service_principal" "sp" {
   provider     = databricks.mws
